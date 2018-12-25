5403424415245fb87f640718ee6778334f6bcdeb
Stefan Miljevic <stefi5miljevic@gmail.com>
2018-12-25 21:10:54 +0100

    fh info page update

--- public_html/script/google-map.js
+++ public_html/script/google-map.js
@@ -1,9 +1,6 @@
 function initMap() {
-  // The location of Uluru
   var aUluru = {lat: 48.2392831, lng: 16.3773241};
-  // The map, centered at Uluru
   var aMap = new google.maps.Map(
       document.getElementById('map'), {zoom: 15, center: aUluru});
-  // The marker, positioned at Uluru
   var aMarker = new google.maps.Marker({position: aUluru, map: aMap});
 }
