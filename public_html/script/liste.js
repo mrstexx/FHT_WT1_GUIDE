d2a6908df2ac2f18a710ecf3d4449933b1b941c0
Stefan Miljevic <stefi5miljevic@gmail.com>
2018-12-30 13:19:08 +0100

    list and name search first step

--- public_html/script/liste.js
+++ public_html/script/liste.js
@@ -1,43 +1,38 @@
-var jsonList;
-var inhalt1 = [];
-var inhalt2 = [];
+var listOfRooms = [];
 
-$(function (){
-console.log( "ready!" );
-$.ajax({ 
-  type : 'GET',
-  dataType: 'json',
-  url : '../../rooms.json', 
-  success : (result) => {
-            $.each(data, (objkey, objval) => {
-                inhalt1.push({ key: objkey, value: objval })
+$(function () {
+    console.log("ready!");
+
+    var roomsObject = [];
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: '../res/data/rooms.json',
+        success: function (result) {
+            $.each(result, function (objkey, objval) {
+                roomsObject.push({key: objkey, value: objval})
             });
-            $.each(inhalt[0].value, (objval, objkey) => {
-                inhalt2.push({ key: objkey, value: objval })
-            })
- },
+            $.each(roomsObject[0].value, function (objval, objkey) {
+                listOfRooms.push({key: objval, value: objkey})
+            });
+        },
+        error: function () {
+            console.warn("Problem occured!");
+        }
+    });
 });
-})
 
-function setListe(){
-		var elementlist = document.createElement("ul");
-		elementlist.setAttribute("id", "setlistjson");
-		
-		//eine eigene var für jedes li
-		for(var y = 0; y < inhalt2.length ; y++)
-		{
-			var elementlisteliy = document.createElement("li");
-		}
-		
-		//damit jedes li eine unique id hat
-		for(var x = 0; x < inhalt2.length ; x++)
-		{
-			elementlisteli[x].setAttribute("id", "elementlisteli"[x]);
-		}
-		
-		for(var z = 0; z < inhalt2.length ; z++)
-		{
-			document.getElementById('elementlisteli'[z]).appendChild(elementlisteli[z]);
-		}
-		
+$('#listSearch').on('click', function () {
+    $('#raumbelegung-structure').empty ();
+    $('#raumbelegung-structure').prepend(setList());
+});
+
+function setList() {
+    var listStructure = '<div class="form-group"><select class="form-control mx-auto" id="listSelection"><option selected>Wählen Sie Raum aus</option>';
+    for (var i = 0; i < listOfRooms.length; i++)
+    {
+        listStructure+='<option>' + listOfRooms[i].value + '</option>';
+    }
+    listStructure += '</select></div>';
+    return listStructure;
 }
