2603eeb9cfddcdadccb1468131cff55a13b1a5a5
Stefan Miljevic <stefi5miljevic@gmail.com>
2019-01-03 17:45:41 +0100

    qr scanner implementation

--- public_html/res/js/qr-code.js
+++ public_html/res/js/qr-code.js
@@ -1,37 +1,77 @@
-$(document).ready(function() {
-	// for webcam support
-	$('#example').photobooth().on("image", function(event, dataUrl) {
-		//$("#hiddenImg").html('<img src="' + dataUrl + '" >');
-		qrCodeDecoder(dataUrl);
-		//console.log(event);
-		//console.log(dataUrl);
-		//console.log($('#example').data( "photobooth" ));
-	});
-
-	$('#qrScan').click(function() {
-		$('.trigger').trigger('click');
-    });
-
+$(document).ready(function () {
     $('#qrCodeSearch').on('click', function () {
         $('#table-results').empty();
+        $('#raumbelegung-structure .wrong-name').empty();
         $('#raumbelegung-structure').empty();
         $('#table-results_wrapper').hide();
-        $('#raumbelegung-structure').prepend('<div class="boxWrapper">' +
-			'<div id="example"></div></div><div class="btn btn-primary">' +
-			'<a id="qrScan">Scan QR code</a></div><div class="boxWrapper auto">' +
-			'<div id="hiddenImg"></div><div id="qrContent"><p>Result will be here.</p>' +
-			'</div></div>');
+        $('#raumbelegung-structure').prepend('<button class="btn btn-primary" id="qr-button">Take new photo</button>' +
+            '<div class="video-container">' +
+            '<video id="video-preview"></video>' +
+            '<canvas id="qr-canvas" class="hidden"></canvas>' +
+            '</div>');
+        newScan();
     });
-    
-	qrcode.callback = showInfo;
+
+    $('#raumbelegung-structure').on('click', '#qr-button', function () {
+        $('#raumbelegung-structure .wrong-name').empty();
+        $('#table-results').empty();
+        $('#table-results_wrapper').hide();
+        $('#video-preview').removeClass("hidden");
+        $('#qr-canvas').addClass("hidden");
+        newScan();
+    });
 });
 
-// decode the imgage
-function qrCodeDecoder(dataUrl) {
-	qrcode.decode(dataUrl);
+function newScan() {
+    navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}, audio: false}).then(function (stream) {
+        var video = document.getElementById("video-preview");
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true); /* otherwise iOS safari starts fullscreen */
+        video.play();
+        setTimeout(tick, 100); /* We launch the tick function 100ms later (see next step) */
+    }).catch(function (err) {
+        console.error(err);
+    });
 }
 
-// show results from qr code
-function showInfo(data) {
-	$("#qrContent p").text(data);
+function tick() {
+    var video = document.getElementById("video-preview");
+    var qrCanvasElement = document.getElementById("qr-canvas");
+    var qrCanvas = qrCanvasElement.getContext("2d");
+
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        qrCanvasElement.height = video.videoHeight;
+        qrCanvasElement.width = video.videoWidth;
+        qrCanvas.drawImage(video, 0, 0, qrCanvasElement.width, qrCanvasElement.height);
+        try {
+            var result = qrcode.decode();
+            /* Video can now be stopped */
+            video.pause();
+            video.src = "";
+            video.srcObject.getVideoTracks().forEach(function (track) {
+                track.stop()
+            });
+
+            /* Display Canvas and hide video stream */
+            qrCanvasElement.classList.remove("hidden");
+            video.classList.add("hidden");
+
+            /* Display raumbelegung */
+            if (isValidName(result)) {
+                var roomNumber = result.replace(/[^0-9]/g, '');
+                loadRoomData(roomNumber);
+            } else {
+                $('#raumbelegung-structure').append('<h6 class="wrong-name">Raumname mit eingegebenem QR-Code existiert nicht.</h6>');
+            }
+        } catch (e) {
+            /* No Op */
+        }
+    }
+
+    /* If no QR could be decoded from image copied in canvas */
+    if (!video.classList.contains("hidden")) {
+        setTimeout(tick, 100);
+    }
 }
+
+
